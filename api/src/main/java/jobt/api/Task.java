c0a9e54a4a8ff4a3253a1f30950a9b9bc171b28c
Oliver Siegmar <oliver@siegmar.de>
2017-06-21 15:53:31 +0200

    add javadoc

--- api/src/main/java/jobt/api/Task.java
+++ api/src/main/java/jobt/api/Task.java
@@ -3,14 +3,21 @@ package jobt.api;
 /**
  * <strong>Performance consideration:</strong> A task is registered, if the plugin it's belonging
  * to, is specified in the build configuration &ndash; even if it isn't used in the build process.
- * Ensure quick initialization and put time-consuming code in {@link #run()}.
+ * Ensure quick initialization and put time-consuming code in {@link #prepare()} and {@link #run()}.
  *
  * @see jobt.api.AbstractPlugin
  */
 public interface Task {
 
+    /**
+     * Prepare for run. This method might be called before the task dependencies are met.
+     * The prepare method of other tasks might be called in parallel.
+     */
     void prepare() throws Exception;
 
+    /**
+     * This method is called as soon as all dependencies are met.
+     */
     TaskStatus run() throws Exception;
 
 }

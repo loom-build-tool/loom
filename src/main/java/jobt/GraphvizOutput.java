6e2b7a1096e9d95da43ffcee338d065cf4391e42
cc-cpo <codecave@gmx.net>
2017-06-29 14:34:22 +0200

    graphviz: show products and dependences

--- src/main/java/jobt/GraphvizOutput.java
+++ src/main/java/jobt/GraphvizOutput.java
@@ -1,15 +1,14 @@
 package jobt;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import jobt.plugin.ConfiguredTask;
@@ -40,49 +39,38 @@ public final class GraphvizOutput {
 
     private static void writeTasks(final TaskRegistryLookup taskRegistryLookup,
                                    final PrintWriter pw) {
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        final PrintWriter graphBuffer = new PrintWriter(out);
+
         pw.println("digraph dependencies {");
         pw.println("    rankdir=\"RL\";");
         pw.println("    graph [splines=spline, nodesep=1];");
+        pw.println("    node [shape=box];");
 
-        final Set<String> allProducts = new HashSet<>();
+        for (final String taskName : taskRegistryLookup.getTaskNames()) {
 
-        final Set<String> allTaskNames = taskRegistryLookup.getTaskNames();
-        for (final String taskName : allTaskNames) {
-            final ConfiguredTask configuredTask = taskRegistryLookup.lookupTask(taskName);
-            final List<String> providedProducts =
-                configuredTask.getProvidedProducts().stream()
-                .map(p -> "produced_" + p) // FIXME
-                .collect(Collectors.toList());
+            final ConfiguredTask task = taskRegistryLookup.lookupTask(taskName);
 
-            final List<String> usedProducts = configuredTask.getUsedProducts().stream()
-                .map(p -> "product_" + taskName + "__" + p) // FIXME
-                .collect(Collectors.toList());
+            for (final String productId :  task.getProvidedProducts()) {
 
-            allProducts.addAll(providedProducts);
-            allProducts.addAll(usedProducts);
+                writeKeyValue(pw, productId,
+                    productId + "\\n" + taskName, Collections.emptyList());
 
-            // task -> provided products
-            writeKeyValue(graphBuffer, taskName, providedProducts);
+                writeKeyValue(pw, productId, null,
+                    new ArrayList<>(task.getUsedProducts()));
 
-            // task -> used products
-            writeKeyValue(graphBuffer, taskName, usedProducts);
+            }
+
         }
-
-        pw.println("    node [shape=rectangle]; " + String.join(";", allTaskNames));
-        pw.println("    node [shape=oval]; " + String.join(";", allProducts));
-
-        graphBuffer.flush();
-        pw.print(out.toString());
 
         pw.println("}");
     }
 
-    private static void writeKeyValue(final PrintWriter pw, final String key,
+    private static void writeKeyValue(final PrintWriter pw, final String key, final String label,
                                       final List<String> values) {
         pw.print("    ");
         pw.print(key);
+        if (label != null) {
+            pw.print(" [label=\"" + label + "\"] ");
+        }
         if (!values.isEmpty()) {
             pw.print(" -> ");
             pw.print(constructValue(values));

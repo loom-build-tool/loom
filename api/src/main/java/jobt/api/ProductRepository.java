183d9d8076ba5c69bc16258255373dbd43eebf02
cc-cpo <codecave@gmx.net>
2017-06-30 08:26:46 +0200

    list products; refactor inversemap (#15)

--- api/src/main/java/jobt/api/ProductRepository.java
+++ api/src/main/java/jobt/api/ProductRepository.java
@@ -1,13 +1,9 @@
 package jobt.api;
 
-import java.util.Set;
-
 public interface ProductRepository {
 
     ProductPromise lookup(String productId);
 
     void createProduct(String productId);
-
-    Set<String> getProductNames();
 
 }

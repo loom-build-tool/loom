aa15b70f1e8dc408a39a3df8dd296c35f3bc0382
Oliver Siegmar <oliver@siegmar.de>
2017-07-06 15:10:27 +0200

    create target dir if it doesn't exist

--- local_install.sh
+++ local_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 VERSION=1.0.0
 TARGET=~/.loom/binary/
+test -d $TARGET || mkdir -p $TARGET
 ./gradlew clean zip &&
     rm -rf $TARGET/loom-$VERSION &&
     unzip build/distributions/loom-$VERSION.zip -d $TARGET

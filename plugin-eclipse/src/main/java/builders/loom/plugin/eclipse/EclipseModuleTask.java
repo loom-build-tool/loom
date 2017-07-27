a303f0d91e80736c5f54049213e3560b4fc279da
cc-cpo <codecave@gmx.net>
2017-07-27 11:15:42 +0200

    eclispe java9 module support

--- plugin-eclipse/src/main/java/builders/loom/plugin/eclipse/EclipseModuleTask.java
+++ plugin-eclipse/src/main/java/builders/loom/plugin/eclipse/EclipseModuleTask.java
@@ -25,7 +25,16 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
+import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -45,16 +54,20 @@ import org.xml.sax.SAXException;
 
 import builders.loom.api.AbstractTask;
 import builders.loom.api.JavaVersion;
+import builders.loom.api.Module;
+import builders.loom.api.ModuleGraphAware;
 import builders.loom.api.TaskResult;
 import builders.loom.api.product.ArtifactListProduct;
 import builders.loom.api.product.ArtifactProduct;
 import builders.loom.api.product.DummyProduct;
+import builders.loom.util.xml.XmlBuilder;
 
 @SuppressWarnings("checkstyle:classfanoutcomplexity")
-public class EclipseModuleTask extends AbstractTask {
+public class EclipseModuleTask extends AbstractTask implements ModuleGraphAware {
 
     private final DocumentBuilder docBuilder;
     private final Transformer transformer;
+    private Map<Module, Set<Module>> moduleGraph;
 
     public EclipseModuleTask() {
         try {
@@ -70,12 +83,17 @@ public class EclipseModuleTask extends AbstractTask {
         }
     }
 
+    private Set<Module> allModules() {
+        return moduleGraph.keySet();
+    }
+
     private static InputStream readResource(final String resourceName) {
         return new BufferedInputStream(EclipseModuleTask.class.getResourceAsStream(resourceName));
     }
 
     @Override
     public TaskResult run() throws Exception {
+
         final Path currentDir = Paths.get("");
 
         final Path currentWorkDirName = currentDir.toAbsolutePath().getFileName();
@@ -84,21 +102,46 @@ public class EclipseModuleTask extends AbstractTask {
             throw new IllegalStateException("Can't get current working directory");
         }
 
-        final String projectName = currentWorkDirName.toString();
-        writeDocumentToFile(currentDir.resolve(".project"), createProjectFile(projectName));
-        writeDocumentToFile(currentDir.resolve(".classpath"), createClasspathFile());
+        for (final Module module : allModules()) {
+            createModuleProject(module);
+        }
 
         return completeOk(new DummyProduct("Eclipse project files"));
     }
 
+    private void createModuleProject(final Module module)
+        throws IOException, SAXException, TransformerException, InterruptedException {
+        writeDocumentToFile(module.getPath().resolve(".project"), createProjectFile(module));
+        writeDocumentToFile(module.getPath().resolve(".classpath"), createClasspathFile(module));
+        final Path settingsPath = Files.createDirectories(module.getPath().resolve(".settings"));
+        writePropertiesToFile(settingsPath.resolve("org.eclipse.jdt.core.prefs"), createJdtPrefs(module));
+    }
+
+    private Properties createJdtPrefs(Module module) {
+
+        Properties prefs = new Properties();
+        String javaLangLevel = buildProjectJdkName(module.getConfig().getBuildSettings().getJavaPlatformVersion());
+
+        prefs.setProperty("eclipse.preferences.version", "1");
+        prefs.setProperty("org.eclipse.jdt.core.compiler.source", javaLangLevel);
+        prefs.setProperty("org.eclipse.jdt.core.compiler.compliance", javaLangLevel);
+
+        return prefs;
+    }
+
+    private static String buildProjectJdkName(final JavaVersion javaVersion) {
+        final int numericVersion = javaVersion.getNumericVersion();
+        return (numericVersion < 9) ? "1." + numericVersion : String.valueOf(numericVersion);
+    }
+
     @SuppressWarnings("checkstyle:magicnumber")
-    private Document createProjectFile(final String name) throws IOException, SAXException {
+    private Document createProjectFile(final Module module) throws IOException, SAXException {
         try (final InputStream resourceAsStream = readResource("/project-template.xml")) {
             final Document doc = docBuilder.parse(resourceAsStream);
             final Node component = doc.getDocumentElement()
                 .getElementsByTagName("name").item(0);
 
-            component.setTextContent(name);
+            component.setTextContent(module.getModuleName());
 
             return doc;
         }
@@ -114,66 +157,93 @@ public class EclipseModuleTask extends AbstractTask {
         }
     }
 
+    private void writePropertiesToFile(Path file, Properties properties) throws IOException {
+
+        try (final OutputStream outputStream = newOut(file)) {
+            properties.store(outputStream, "Loom Eclipse Plugin");
+        }
+
+    }
+
     private OutputStream newOut(final Path file) throws IOException {
         return new BufferedOutputStream(Files.newOutputStream(file,
             StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING));
     }
 
     @SuppressWarnings("checkstyle:magicnumber")
-    private Document createClasspathFile()
+    private Document createClasspathFile(Module module)
         throws IOException, InterruptedException, SAXException {
 
-        try (final InputStream resourceAsStream = readResource("/classpath-template.xml")) {
-            final Document doc = docBuilder.parse(resourceAsStream);
-            final Element root = doc.getDocumentElement();
+        final XmlBuilder.Element rootBuilder = XmlBuilder
+            .root("classpath");
 
-            final String projectJdkName;
-            final int javaPlatformVersion = JavaVersion.current() // FIXME
-                .getNumericVersion();
+        addSourceDirIfExists(rootBuilder, module.getPath(), Paths.get("src", "main", "java"));
+        addSourceDirIfExists(rootBuilder, module.getPath(), Paths.get("src", "main", "resources"));
+        addSourceDirIfExists(rootBuilder, module.getPath(), Paths.get("src", "test", "java"));
+        addSourceDirIfExists(rootBuilder, module.getPath(), Paths.get("src", "test", "resources"));
 
-            if (javaPlatformVersion < 9) {
-                projectJdkName = "1." + javaPlatformVersion;
-            } else {
-                projectJdkName = String.valueOf(javaPlatformVersion);
-            }
+        final Document doc = rootBuilder.element("classpathentry")
+            .attr("kind", "output")
+            .attr("path", "bin")
+            .getDocument();
 
-            final Element jdkEntry = doc.createElement("classpathentry");
-            jdkEntry.setAttribute("kind", "con");
-            jdkEntry.setAttribute("path",
-                String.format("org.eclipse.jdt.launching.JRE_CONTAINER/"
-                    + "org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-%s/",
-                    projectJdkName));
-            root.appendChild(jdkEntry);
+        final Element root = doc.getDocumentElement();
 
-            final Optional<ArtifactListProduct> testArtifacts =
-                useProduct("testArtifacts", ArtifactListProduct.class);
+        final Element jdkEntry = doc.createElement("classpathentry");
+        jdkEntry.setAttribute("kind", "con");
+        jdkEntry.setAttribute("path",
+            String.format("org.eclipse.jdt.launching.JRE_CONTAINER/"
+                + "org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-%s/",
+                buildProjectJdkName(module.getConfig().getBuildSettings().getJavaPlatformVersion())));
+        root.appendChild(jdkEntry);
 
-            testArtifacts.ifPresent(artifactListProduct -> {
-                for (final ArtifactProduct path : artifactListProduct.getArtifacts()) {
-                    final String jar = path.getMainArtifact().toAbsolutePath().toString();
-                    final Path sourceArtifact = path.getSourceArtifact();
-                    final String sourceJar = sourceArtifact != null
-                        ? sourceArtifact.toAbsolutePath().toString() : null;
+        for (final Module depModule : moduleGraph.get(module)) {
+            final Element projectRef = doc.createElement("classpathentry");
+            projectRef.setAttribute("combineaccessrules", "false");
+            projectRef.setAttribute("kind", "src");
+            projectRef.setAttribute("path", "/" + depModule.getModuleName());
+            root.appendChild(projectRef);
+        }
 
-                    root.appendChild(buildClasspathElement(doc, jar, sourceJar));
-                }
+        final Optional<ArtifactListProduct> testArtifacts =
+            useProduct(module.getModuleName(),"testArtifacts", ArtifactListProduct.class);
+
+            testArtifacts.map(ArtifactListProduct::getArtifacts).orElse(Collections.emptyList()).stream()
+            .forEach(artifactProduct -> {
+                root.appendChild(buildClasspathElement(doc, artifactProduct));
             });
 
-            return doc;
+        return doc;
+    }
+
+    private void addSourceDirIfExists(XmlBuilder.Element root, final Path modulePath, Path path) {
+        if (Files.exists(modulePath.resolve(path))) {
+            root.element("classpathentry")
+                .attr("kind", "src")
+                .attr("path", path.toString());
         }
     }
 
     private Element buildClasspathElement(final Document doc,
-                                          final String jar, final String sourceJar) {
+                                          ArtifactProduct artifactProduct) {
+
+        final String jar = artifactProduct.getMainArtifact().toAbsolutePath().toString();
+        final Path sourceArtifact = artifactProduct.getSourceArtifact();
+        final Optional<String> sourceJar =
+            Optional.ofNullable(sourceArtifact)
+                .map(Path::toAbsolutePath)
+                .map(Path::toString);
 
         final Element classpathentry = doc.createElement("classpathentry");
-        if (sourceJar != null) {
-            classpathentry.setAttribute("sourcepath", sourceJar);
-        }
+        sourceJar.ifPresent(path -> classpathentry.setAttribute("sourcepath", path));
         classpathentry.setAttribute("kind", "lib");
         classpathentry.setAttribute("path", jar);
 
         return classpathentry;
     }
 
+    @Override
+    public void setTransitiveModuleGraph(final Map<Module, Set<Module>> moduleGraph) {
+        this.moduleGraph = moduleGraph;
+    }
 }

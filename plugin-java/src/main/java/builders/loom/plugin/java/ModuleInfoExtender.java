1eaabb3d9b867d9500e0e3894ea49484382d4b32
Oliver Siegmar <oliver@siegmar.de>
2017-08-20 16:49:03 +0200

    refactoring

--- plugin-java/src/main/java/builders/loom/plugin/java/ModuleInfoExtender.java
+++ plugin-java/src/main/java/builders/loom/plugin/java/ModuleInfoExtender.java
@@ -16,108 +16,80 @@
 
 package builders.loom.plugin.java;
 
-import java.io.Closeable;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.objectweb.asm.Attribute;
 import org.objectweb.asm.ByteVector;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Label;
 import org.objectweb.asm.Opcodes;
 
 final class ModuleInfoExtender {
-
-    private static final String MODULE_MAIN_CLASS = "ModuleMainClass";
 
     private ModuleInfoExtender() {
     }
 
     static byte[] extend(final byte[] data, final String mainClassName) {
+        Objects.requireNonNull(data, "data required");
+        Objects.requireNonNull(mainClassName, "mainClassName required");
+
         final ClassWriter cw =
             new ClassWriter(ClassWriter.COMPUTE_MAXS + ClassWriter.COMPUTE_FRAMES);
 
-        try (final AttributeAddingClassVisitor cv =
-                 new AttributeAddingClassVisitor(Opcodes.ASM6, cw)) {
+        final List<Attribute> attributes = List.of(new ModuleMainClassAttribute(mainClassName));
 
-            if (mainClassName != null) {
-                cv.addAttribute(new ModuleMainClassAttribute(mainClassName));
-            }
+        final AttributeAddingClassVisitor cv =
+            new AttributeAddingClassVisitor(Opcodes.ASM6, cw, attributes);
 
-            final Attribute[] attrs = new Attribute[] {
-                new ModuleMainClassAttribute(),
-            };
+        new ClassReader(data).accept(cv, 0);
 
-            new ClassReader(data).accept(cv, attrs, 0);
-        }
+        attributes.forEach(cv::visitAttribute);
 
         return cw.toByteArray();
     }
 
-    private static class AttributeAddingClassVisitor extends ClassVisitor implements Closeable {
+    private static class AttributeAddingClassVisitor extends ClassVisitor {
 
         private final Map<String, Attribute> attrs = new HashMap<>();
 
-        AttributeAddingClassVisitor(final int api, final ClassVisitor cv) {
+        AttributeAddingClassVisitor(final int api, final ClassVisitor cv,
+                                    final List<Attribute> attributes) {
             super(api, cv);
-        }
-
-        void addAttribute(final Attribute attr) {
-            attrs.put(attr.type, attr);
+            attributes.forEach(a -> attrs.put(a.type, a));
         }
 
         @Override
-        public void visitAttribute(final Attribute attr) {
-            final String name = attr.type;
-
-            final Attribute replacement = attrs.get(name);
-            if (replacement == null) {
-                super.visitAttribute(attr);
-                return;
-            }
-
-            super.visitAttribute(replacement);
-            attrs.remove(name);
-        }
-
-        @Override
-        public void close() {
-            attrs.values().forEach(super::visitAttribute);
-            attrs.clear();
+        public void visitAttribute(final Attribute oldAttr) {
+            final Attribute newAttr = attrs.get(oldAttr.type);
+            super.visitAttribute(newAttr == null ? oldAttr : newAttr);
         }
 
     }
 
     private static class ModuleMainClassAttribute extends Attribute {
 
-        private final String mainClass;
+        private static final String MODULE_MAIN_CLASS = "ModuleMainClass";
 
-        ModuleMainClassAttribute(final String mainClass) {
+        private final String mainClassName;
+
+        ModuleMainClassAttribute(final String mainClassName) {
             super(MODULE_MAIN_CLASS);
-            this.mainClass = mainClass;
-        }
-
-        ModuleMainClassAttribute() {
-            this(null);
-        }
-
-        @Override
-        protected Attribute read(final ClassReader cr, final int off, final int len,
-                                 final char[] buf, final int codeOff, final Label[] labels) {
-            final String mainClassName = cr.readClass(off, buf).replace('/', '.');
-            return new ModuleMainClassAttribute(mainClassName);
+            this.mainClassName = mainClassName;
         }
 
         @Override
         protected ByteVector write(final ClassWriter cw, final byte[] code, final int len,
                                    final int maxStack, final int maxLocals) {
             final ByteVector v = new ByteVector();
-            final int index = cw.newClass(mainClass.replace('.', '/'));
+            final int index = cw.newClass(mainClassName.replace('.', '/'));
             v.putShort(index);
             return v;
         }
+
     }
 
 }

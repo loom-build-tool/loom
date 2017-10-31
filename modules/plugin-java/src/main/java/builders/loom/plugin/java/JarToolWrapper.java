a37294c552f1fb1a0e70f0c9e0fcd1910e91e795
Oliver Siegmar <oliver@siegmar.de>
2017-10-31 16:36:36 +0100

    refactor

--- modules/plugin-java/src/main/java/builders/loom/plugin/java/JarToolWrapper.java
+++ modules/plugin-java/src/main/java/builders/loom/plugin/java/JarToolWrapper.java
@@ -19,13 +19,20 @@ package builders.loom.plugin.java;
 import java.util.Collection;
 import java.util.spi.ToolProvider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class JarToolWrapper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JarToolWrapper.class);
 
     private final ToolProvider toolProvider = ToolProvider.findFirst("jar")
         .orElseThrow(() -> new IllegalStateException("Couldn't find jar ToolProvider"));
 
     @SuppressWarnings("checkstyle:regexpmultiline")
     void jar(final Collection<String> args) {
+        LOG.debug("Run JarToolProvider with args: {}", args);
+
         final int result = toolProvider.run(System.out, System.err, args.toArray(new String[]{}));
 
         if (result != 0) {
